b1290fb97ed6024d524b855b947fb86729c32e10
Soniya Khan <71494542+Rizv123@users.noreply.github.com>
2023-07-18 20:21:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+let items = ["Learn DSA","Do DSA" ,"Practice DSA" ,"Become master DSA"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded(({extended:true})));
+app.use(express.static("public"));
+
+
+app.get("/" , function(req, res){
+
+  let day = date.getDay();
+   
+    res.render("list",{listTitle : day , newListItems : items});
+});
+
+
+app.post("/" ,function(req, res){
+
+  let item = req.body.newItem; 
+
+  if(req.body.list === "Work"){
+     workItems.push(item);
+     res.redirect("/work");
+   } else{
+     items.push(item);
+     res.redirect("/");
+   }
+    
+});
+
+
+app.get("/work", function(req, res){
+   res.render("list", {listTitle:"Work List", newListItems: workItems});
+});
+
+app.post("/work",function(req, res){
+    let item = req.body,newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.listen(7000, function(){
+    console.log("server will serve on 7000 port");
+});
